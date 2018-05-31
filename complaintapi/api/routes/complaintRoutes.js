9e567ff7afbc7fbdfb6ab7df73da49ea29524a1e
Paavo Väänänen <vaananen.paavo@gmail.com>
2018-05-31 13:58:47 +0200

    working stuff

--- complaintapi/api/routes/complaintRoutes.js
+++ complaintapi/api/routes/complaintRoutes.js
@@ -1,57 +1,78 @@
 // complaintRoutes.js
 'use strict';
-const Worker = require('camunda-worker-node'),
-    Backoff = require('camunda-worker-node/lib/backoff'),
-    Prompt = require('prompt');
 
-const engineEndpoint = 'http://localhost:8080/engine-rest/';
+const   Worker = require('camunda-worker-node'),
+        Backoff = require('camunda-worker-node/lib/backoff'),
+        rp = require('request-promise');
 
-const processComplaint = Worker(engineEndpoint, {
-    workerId: 'process-complaint',
+const   engineEndpoint = 'http://35.158.119.79:8080/engine-rest/';
+
+const options = {
+        uri : 'http://35.158.119.79:8080/engine-rest/user?memberOfGroup=taxback',
+        json: true
+    }
+
+const assignUser = Worker(engineEndpoint, {
+    workerId: 'assign-user',
     use: [
         Backoff
     ]
 });
 
 module.exports = function(app) {
-
-    processComplaint.subscribe('process-complaint', async () => {
-        return {
-            variables: {
-            complaintAccepted: isAccepted(),
-            }
-        };
+    assignUser.subscribe('assign-user', async () => {
+       var member = await getGroup();
+       console.log(member);
+       // member = await getGroup();
+       return {
+           variables: {
+               assignee: member,
+               status: 'inReview'
+           }
+       }
+        // return selectUser().then((member) => {
+        //     console.log(member);
+        //     return {
+        //         variables: {
+        //             assignee: member
+        //         }
+        //     };
+        // }).catch((e) => {
+        //     return {
+        //         variables: {
+        //             assignee: 'virhe'
+        //         }
+        //     };
+        // });
     });
-
 };
 
-function isAccepted() {
-    var isAccept = false;
-
-    //This is to mock the admin actions in the admin-page
-    Prompt.start();
-
-    console.log('You have a pending complaint. Do you want to accept Y/N?')
-
-    Prompt.get(['accept'], function (err, result) {
-        if(result.accept === 'y') {
-            isAccept = true
-            console.log('Complaint accepted')
-        } else {
-            console.log('Complaint NOT accepted')
-        }
-      });
-    return isAccept;
+async function getGroup() {
+    var group = [];
+    console.log('Fetching group...');
+    return rp(options)
+        .then(function (res) {
+            var obj = res;
+            for (var i in obj)
+                group.push(obj[i].id);
+                return selectUser(group);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
-    // app.post('/complaint', (req, res) => {
-    //     var complaint = req.body;
+async function selectUser(group) {
+    console.log('Selecting user from group: ' + group);
+    
+    var user = group[Math.floor(Math.random() * group.length)];
+    console.log(user);
 
-    //     if (complaint.taxBack > 500) {
-    //         complaint.complaintStatus = 'rejected'
-    //         res.send(complaint)
-    //     }
+    return user;
+}
 
-    //     complaint.complaintStatus = 'accepted'
-    //     res.send(complaint)
-    //   });
+// var group = [];
+//         var obj =  JSON.parse(data);
+//         for (var i in obj)
+//             group.push(obj[i].id);
+//             return selectUser(group);
